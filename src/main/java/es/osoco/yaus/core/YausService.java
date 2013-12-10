f1467291a32ed407d6611ca86a965715f29fc422
Paco Soberón <unratito@gmail.com>
2013-12-10 23:07:35 +0100

    Core service stub

---
+++ src/main/java/es/osoco/yaus/core/YausService.java
@@ -0,0 +1,14 @@
+package es.osoco.yaus.core;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class YausService {
+    public String getURL(String hash) {
+        throw new UnsupportedOperationException();
+    }
+
+    public String addURL(String url) {
+        throw new UnsupportedOperationException();
+    }
+}
