4c428b321bc7ff06d8a0fcda68909a142ece0a80
Paco Soberón <unratito@gmail.com>
2013-12-10 23:44:24 +0100

    Implement methods of core service

--- src/main/java/es/osoco/yaus/core/YausService.java
+++ src/main/java/es/osoco/yaus/core/YausService.java
@@ -1,14 +1,37 @@
 package es.osoco.yaus.core;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import es.osoco.yaus.persistence.Entry;
+import es.osoco.yaus.persistence.EntryDao;
 
 @Stateless
 public class YausService {
+    @Inject
+    private EntryDao entryDao;
+
     public String getURL(String hash) {
-        throw new UnsupportedOperationException();
+        Entry entry = entryDao.findByHash(hash);
+
+        if (entry != null) {
+            return entry.getUrl();
+        }
+
+        return null;
     }
 
     public String addURL(String url) {
-        throw new UnsupportedOperationException();
+        Entry entry = entryDao.findByUrl(url);
+
+        if (entry != null) {
+            return entry.getHash();
+        }
+
+        String hash = "XXX"; // TODO: generate hash
+
+        entryDao.insert(url, hash);
+
+        return hash;
     }
 }
