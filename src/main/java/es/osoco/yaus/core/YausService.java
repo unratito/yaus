13aa9984f90b07268aab14d84a7eddaf94df8d65
Paco Soberón <unratito@gmail.com>
2013-12-12 23:15:40 +0100

    Add JAX-RS functionality

--- src/main/java/es/osoco/yaus/core/YausService.java
+++ src/main/java/es/osoco/yaus/core/YausService.java
@@ -2,16 +2,26 @@ package es.osoco.yaus.core;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 import es.osoco.yaus.persistence.Entry;
 import es.osoco.yaus.persistence.EntryDao;
 
 @Stateless
+@Path("/")
 public class YausService {
     @Inject
     private EntryDao entryDao;
 
-    public String getUrl(String hash) {
+    @GET
+    @Path("{hash}")
+    @Produces("text/plain")
+    public String getUrl(@PathParam("hash") String hash) {
         Entry entry = entryDao.findByHash(hash);
 
         if (entry != null) {
@@ -21,7 +31,9 @@ public class YausService {
         return null;
     }
 
-    public String addUrl(String url) {
+    @POST
+    @Produces("text/plain")
+    public String addUrl(@FormParam("url") String url) {
         Entry entry = entryDao.findByUrl(url);
 
         if (entry == null) {
---
+++ src/main/java/es/osoco/yaus/rest/YausApplication.java
@@ -0,0 +1,9 @@
+package es.osoco.yaus.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class YausApplication extends Application {
+    // Empty class to activate JAX-RS
+}
